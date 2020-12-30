93af28a07055db7ac559cdaa9f99342528de9114
CygLiDAR <74406474+CygLiDAR@users.noreply.github.com>
2020-12-30 14:04:15 +0900

    Update delete_udev_rules.sh

--- scripts/delete_udev_rules.sh
+++ scripts/delete_udev_rules.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "delete remap the device serial port(ttyUSBX) to laser"
-echo "sudo rm   /etc/udev/rules.d/laser.rules"
-sudo rm   /etc/udev/rules.d/laser.rules
+echo "delete remap the device serial port(ttyUSBX) to cyglidar"
+echo "sudo rm   /etc/udev/rules.d/cyglidar.rules"
+sudo rm   /etc/udev/rules.d/cyglidar.rules
 echo " "
 echo "Restarting udev"
 echo ""
