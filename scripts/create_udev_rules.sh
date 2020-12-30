4ecd3359ebb8ad2ec471d2cfa3c4c9f4e571cdf8
CygLiDAR <74406474+CygLiDAR@users.noreply.github.com>
2020-12-30 14:03:48 +0900

    Update create_udev_rules.sh
    Revised the path

--- scripts/create_udev_rules.sh
+++ scripts/create_udev_rules.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "remap the device serial port(ttyUSBX) to  laser"
-echo "ls01c usb cp210x connection as /dev/laser , check it using the command : ls -l /dev|grep ttyUSB"
-echo "start copy laser.rules to  /etc/udev/rules.d/"
-echo "`rospack find line_laser_ros`/scripts/laser.rules"
-sudo cp `rospack find line_laser_ros`/scripts/laser.rules  /etc/udev/rules.d
+echo "remap the device serial port(ttyUSBX) to  cyglidar"
+echo "ls01c usb cp210x connection as /dev/cyglidar , check it using the command : ls -l /dev|grep ttyUSB"
+echo "start copy cyglidar.rules to  /etc/udev/rules.d/"
+echo "`rospack find cyglidar_pcl`/scripts/cyglidar.rules"
+sudo cp `rospack find cyglidar_pcl`/scripts/cyglidar.rules  /etc/udev/rules.d
 echo " "
 echo "Restarting udev"
 echo ""
